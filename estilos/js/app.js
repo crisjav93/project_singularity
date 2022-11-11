c375cf879cd98b7c90f5db9e34547ff1c8c7b3bb
Cristian Nunes <crisjav2812@gmail.com>
2022-11-10 23:16:52 -0300

    projects page done

--- estilos/js/app.js
+++ estilos/js/app.js
@@ -1,1 +1,18 @@
 $('#carouselFade').carousel();
+
+const cards = document.querySelectorAll('.project_card')
+
+cards.forEach((card) => {
+card.addEventListener('click', () => {
+    const description = card.querySelector('.description')
+    
+    if (!description.hasAttribute('style') && description.scrollHeight > 24) {
+    description.setAttribute('style', `height: ${description.scrollHeight}px`)
+    card.classList.add('-expanded')
+    } else {
+    description.removeAttribute('style')
+    card.classList.remove('-expanded') 
+    }
+    
+})
+})
